524d3fa93ba611853ea64db408bd92137420bc5a
beckym223 <becky.marcu23@gmail.com>
2026-01-13 20:33:44 +0000

    made slurm script for preprocessing and plotting removed

---
+++ slurm_scripts/preprocess_plot_removed_slurm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=stm_plotRemoved
+#SBATCH --output=results/logs/plotRemoved_%A_%a.out
+#SBATCH --error=results/logs/plotRemoved_%A_%a.err
+#SBATCH --array=0-4
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=8G
+#SBATCH --time=02:00:00
+
+module load r-rocker-ml-verse/4.4.0+apptainer
+# Set working directory
+cd ~/cleaner_package || { echo "Failed to change directory to ~/stm_work"; exit 1; }
+
+CHUNKS=(document paragraph page sent_200 sent_500)
+CHUNK_NAME=${CHUNKS[$SLURM_ARRAY_TASK_ID]}
+
+Rscript scripts/make_dfm_and_plotRemoved.R $CHUNK_NAME
