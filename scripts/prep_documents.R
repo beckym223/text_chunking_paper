5e98431823360c79936ebb8327deacc8d17084bd
beckym223 <becky.marcu23@gmail.com>
2026-01-15 16:34:44 +0000

    using get lower thresh function instead of hard coding

--- scripts/prep_documents.R
+++ scripts/prep_documents.R
@@ -9,7 +9,7 @@ if (length(args) != 1) {
     stop("Usage: Rscript prep_documents.R <chunk_name>")
 }
 chunk <- args[[1]]
-
+source("text_funcs.R")
 # ----------------------------
 # Paths
 # ----------------------------
@@ -18,20 +18,10 @@ out_dir  <- file.path("results/prepped", chunk)
 
 dir.create(out_dir, recursive = TRUE, showWarnings = FALSE)
 
-# ----------------------------
-# Thresholds (DEFAULT = 10)
-# ----------------------------
-lower_thresholds <- c(
-    document = 10,
-    paragraph = 20,
-    page = 20,
-    sent_200 = 20,
-    sent_500 = 20
-    
-)
 
-lower_thresh <- lower_thresholds[[chunk]]
-if (is.null(lower_thresh)) lower_thresh <- 10
+
+lower_thresh <- get_lower_thresh(chunk)
+
 
 # ----------------------------
 # Load DFM
