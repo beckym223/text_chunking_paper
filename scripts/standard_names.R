9421aba2c538f86dce16c4169eac573fc3d13ee4
beckym223 <becky.marcu23@gmail.com>
2026-01-26 21:45:40 +0000

    made project environment with standardized paths and labels

---
+++ scripts/standard_names.R
@@ -0,0 +1,88 @@
+
+proj_env<-new.env()
+
+proj_env$create_dir_from_path<-function(path){
+    out.dir<-dirname(path)
+    if(!dir.exists(out.dir)){
+        dir.create(out.dir, recursive = T,showWarnings = F)
+    }
+}
+proj_env$get_lower_thresh <- function(chunk_name) {
+    
+    ## ---------- USER-EDITABLE THRESHOLDS ----------
+    lower_thresholds <- c(
+        document   = 10,
+        paragraph  = 20,
+        page       = 20,
+        sent_200   = 20,
+        sent_500   = 20
+    )
+    ## ---------------------------------------------
+    
+    if (chunk_name %in% names(lower_thresholds)) {
+        lower_thresholds[[chunk_name]]
+    } else {
+        10
+    }
+}
+
+
+
+
+
+
+
+proj_env$chunk_names_path<-"scripts/chunks.txt"
+
+proj_env$load_chunk_names<-function(){
+    chunk_names=readLines(proj_env$chunk_names_path)
+    chunk_names[chunk_names!=""]
+}
+
+proj_env$chunk_display_names<-list(
+        document  = "full document",
+        page      = "page",
+        paragraph = "paragraph",
+        sent_200  = "200 word - nearest sentence",
+        sent_500  = "500 word - nearest sentence",
+        word_200 = "200 word",
+        word_500 = "500 word",
+        word_500_ol = "500 word overlapping"
+    )
+
+
+proj_env$chunk_df_path<-"data/chunked_dfs.rds"
+
+proj_env$get_dfm_path<-function(chunk_name){
+    file.path("results/dfms", chunk_name, "dfm.rds")
+}
+
+proj_env$get_dfm_stm_path<-function(chunk_name){
+    file.path("results/dfms", chunk_name, "dfm_stm.rds")
+}
+
+proj_env$get_plot_removed_path<-function(chunk_name){
+    file.path("results/plotRemoved", chunk_name,"plotRemoved.png")
+}
+
+
+
+proj_env$get_prepped_out_path<-function(chunk_name){
+    file.path("results/prepped", chunk_name, "prepped_documents.rds")
+}
+
+proj_env$get_searchK_out_path<-function(chunk_name){
+    file.path("results/searchK", chunk_name, "searchK_results.rds")
+}
+
+proj_env$get_searchK_plot_path<-function(chunk_name){
+    file.path("results/searchK", chunk_name, "searchK_plot.png")
+}
+
+proj_env$get_chooseK_df_path<-function(chunk_name){
+    file.path("results/chooseK",chunk_name,"chosen_k_df.csv")
+}
+
+proj_env$get_chooseK_plot_path<-function(chunk_name){
+    file.path("results/chooseK",chunk_name,"chosen_k_metric.png")
+}
