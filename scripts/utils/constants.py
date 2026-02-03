a7d5f5e9ef7da6132bc57bffa6f25ca1277bc26e
beckym223 <becky.marcu23@gmail.com>
2026-02-03 20:32:48 +0000

    chunk display names and abbreviations in python

---
+++ scripts/utils/constants.py
@@ -0,0 +1,21 @@
+CHUNK_DISPLAY_NAMES=dict(
+        document  = "full document",
+        page      = "page",
+        paragraph = "paragraph",
+        sent_200  = "200 word: nearest sentence",
+        sent_500  = "500 word: nearest sentence",
+        word_200 = "200 word",
+        word_500 = "500 word",
+        word_500_ol = "500 word overlapping"
+    )
+
+CHUNK_ABBREV=dict(
+    document  = "doc",
+    page      = "pg",
+    paragraph = "par",
+    sent_200  = "sen2",
+    sent_500  = "sen5",
+    word_200 = "w2",
+    word_500 = "w5",
+    word_500_ol = "w5ol"
+)
